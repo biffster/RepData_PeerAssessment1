64ddaa612f4746e1ad6c011118faa2df42d11157
Michael Fierro <mifierro@cisco.com>
2016-01-10 22:21:37 -0700

    Added time series

---
+++ time_series.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(forecast)
+
+testvals <- read.csv("activity.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "NA")
+
+testmsts <- msts(tvals1$steps, seasonal.periods = 24*12)
+applymsts <- tapply(testmsts, cycle(testmsts), mean, na.rm = TRUE)
+plot(applymsts, type = "l", xlab = "Time interval number (1 = midnight, 288 = 11:55pm)", ylab = "Average number of steps")
+dev.copy(png,"5mintimeseries.png")
+dev.off()
